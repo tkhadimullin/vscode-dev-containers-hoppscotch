e1be6c3856b9392a9a9329a8890a11fe6f9a6729
T Khadimulin <khadimulin@gmail.com>
2021-10-03 02:24:37 +1300

    initial commit

---
+++ .devcontainer/library-scripts/dotnet-debian.sh
@@ -0,0 +1,17 @@
+
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+wget https://packages.microsoft.com/config/debian/11/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update; \
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y dotnet-sdk-3.1
